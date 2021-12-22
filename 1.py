9a38a68ee9b664a368b5af298fb335fa4b65c64f
paresh skill qode <skillqode.paresh@gmail.com>
2021-12-22 10:14:46 +0530

    date 2212 second

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+# git remote add origin https://github.com/Qodeskill/ml_9.00am.git
 import numpy as np
 # import matplotlib.pyplot as plt
 # a = np.arange(1,11)
@@ -8,24 +9,24 @@ import numpy as np
 # print(f'size of the array a ==> {a.size}')
 # print(f'shape of the array a ==> {a.shape}')
 
-a = np.arange(1,10).reshape(3,3)
-print(f"Original 2-D array ==> \n {a}")
-print(f'dimensions of the array a ==> {a.ndim}')
+# a = np.arange(1,10).reshape(3,3)
+# print(f"Original 2-D array ==> \n {a}")
+# print(f'dimensions of the array a ==> {a.ndim}')
 # print(f"Elements prsent at 0-diagonal ==> {np.diag(a,k=0)}")
 # print(f"Elements prsent at 1-diagonal ==> {np.diag(a,k=1)}")
 # print(f"Elements prsent at 2-diagonal ==> {np.diag(a,k=2)}")
 # print(f"Elements prsent at -1-diagonal ==> {np.diag(a,k=-1)}")
 # print(f"Elements prsent at -2-diagonal ==> {np.diag(a,k=-2)}")
 # print(f"Elements prsent at 3-diagonal ==> {np.diag(a,k=3)}")
-a = np.array([10,20,30,40])
-print(f'dimensions of the array a ==> {a.ndim}')
+# a = np.array([10,20,30,40])
+# print(f'dimensions of the array a ==> {a.ndim}')
 # print(" array ")
-print(np.diag(a,k=2))
-print(np.diag(a,k=2))
+# print(np.diag(a,k=2))
+# print(np.diag(a,k=2))
 # s = np.random.uniform(20,30,size=1000000)
 
 # a = np.array([10,20,30,40,50])
-# print(a[3])
+# print(a[4])
 # print(a[-1])
 
 # a = np.array([[10,20,30],[40,50,60]])
@@ -35,6 +36,14 @@ print(np.diag(a,k=2))
 # print(f"a[1][-2] ==> {a[1][-2]}")
 # print(f"a[-1][2] ==> {a[-1][-2]}")
 # print(f"a[-1][1] ==> {a[-1][1]}")
+
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(f"Shape of the array a : {a.shape}")
+
+print(f"a[1][-1] ==> {a[1][-1]}")
+print(f"a[-2][-1] ==> {a[-2][-1]}")
+print(f"a[-2][2] ==> {a[-2][2]}")
 
 
 # l = [[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]]
