7bf206a9e0cfbc586ea652725f2ceb76343efe3a
paresh skill qode <skillqode.paresh@gmail.com>
2022-02-12 09:20:53 +0530

    commit 12022022

---
+++ python/mod1.py
@@ -0,0 +1,5 @@
+import module.mymath as m 
+# print(module.mymath.a)
+# print(m.a)
+m.add(10,20)
+print("product : ",m.product(10,20))
---
+++ python/module/mymath.py
@@ -0,0 +1,5 @@
+# a=10
+def add(a,b):
+    print("The Sum:",a+b)
+def product(a,b):
+    return a*b
